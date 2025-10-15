a8ba1f899acc94f0ba49f480282f4a0ea36e490b
zayyankece <mzayyanputra@gmail.com>
2025-10-15 21:27:30 +0800

    Fix EditCommand.java mistake

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -46,7 +46,7 @@ public class EditCommand extends Command {
             + "[" + PREFIX_EMAIL + "EMAIL] "
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_TAG + "TAG]..."
-            + "[" + PREFIX_ROLE + "ADDRESS] \n"
+            + "[" + PREFIX_ROLE + "ROLE] \n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_PHONE + "91234567 "
             + PREFIX_EMAIL + "johndoe@example.com";
