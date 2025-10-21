52b517bd250b02b978c48fbabe28e2844e7b7e8a
tjinyao <tjinyao@gmail.com>
2025-10-22 07:43:47 +0800

    Add stats command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -21,6 +21,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.LogCommand;
+import seedu.address.logic.commands.StatsCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -102,6 +103,9 @@ public class AddressBookParser {
 
         case ExportContactListCommand.COMMAND_WORD:
             return new ExportContactListCommandParser().parse(arguments);
+
+        case StatsCommand.COMMAND_WORD:
+            return new StatsCommand();
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
